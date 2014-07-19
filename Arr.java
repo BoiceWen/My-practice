b4a5b331f7d1742d97d505bb1e6607cbbc789c03
BoiceWen <boice0819@gmail.com>
2014-07-19 20:24:58 +0800

    Update Arr.java

--- Arr.java
+++ Arr.java
@@ -5,39 +5,59 @@ public class Arr
 	public static void main(String[] args)
 	{
 		Scanner scanner = new Scanner(System.in);
-		//int[] arr = new int[]{2,7,0,2,3,8,-6,5};
-		//int[] arr2 = new int[8];
-		int length = 0;
+		Findmax fMax = new Findmax();
+		
+		
 		int max = 0;
 		int input = 0;
 		
 		System.out.print("please key in the array`s length:");
-		length = scanner.nextInt();
-		int[] arr = new int[length];
-		int[] arr2 = new int[length];
+		fMax.setLength(scanner.nextInt()) ;
+		int[] arr = new int[fMax.getLength()];
 		
-		for(int i = 0; i < length; i++)
+		
+		for(int i = 0; i < fMax.getLength(); i++)
 		{
 			System.out.print("please key in the number to the array:");
 			input = scanner.nextInt();
 			arr[i] = input;
 		}
-		
-		
+		fMax.crossArr(arr);
+	}		
+}
+
+class Findmax
+{
+	private int length;
+	int max = 0;
+	
+	public void setLength(int len)
+	{
+		length = len;
+	}
+	public int getLength()
+	{
+		return length;
+	}
+	
+	void crossArr(int[] arr)
+	{
+		int[] arr2 = new int[length];
 		for(int i = 0; i < arr.length ; i++)
 		{
 			if(i == 0)
 			{
 				arr2[0] = arr[0];
 			}
-			else if(arr[i] == 0)
+			else if(arr[i] == 0 )
 			{
 				arr2[i] = 0;
-				if(i != length-1)
+				if(i != (length-1))
 				{
 					arr2[i+1] = arr[i+1];
 				}
 				i = i + 1 ;
+
 			}
 			else
 				arr2[i] = arr[i] * arr2[i-1]; 	
@@ -54,10 +74,12 @@ public class Arr
 			}
 			else if(arr[i] == 0)
 			{
+				
 				arr[i] = 0;
 				if(i != 0)
 				{
 					arr[i-1] = arr[i-1];
+					
 				}
 				i = i - 1 ;
 			}
@@ -65,16 +87,20 @@ public class Arr
 				arr[i] = arr[i] * arr[i+1]; 	
 		}
 		 Arrays.sort(arr);
-		
-		if(arr2[length-1]>arr[length-1] )
-		{	
-			max = arr2[length-1];
-			System.out.println("the Max:"+max);
-		}
-		else
-		{
-			max = arr[length-1];
-			System.out.println("the Max:"+max);
-		}
+		 if(arr2[length-1]>arr[length-1] )
+			{	
+			 	
+				max = arr2[length-1];
+				outputMax(max);
+			}
+			else
+			{
+				max = arr[length-1];
+				outputMax(max);
+			}
+	}
+	void outputMax(int max)
+	{
+		System.out.println("the Max:"+max);
 	}
 }
